04d90066a2b03e6049bdc460a9044d39542ee170
George Baxter <georgebaxter032@gmail.com>
2025-08-04 14:55:11 -0400

    Added dad joke API icanhazdadjoke.com API integrated to display a dad joke on button press below character card. Added User-Agent header as required by API doc Updated Footer with links to API docs

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ const button = document.getElementById('getCharacter');
 const characterInfo = document.getElementById('characterInfo');
 const characterName = document.getElementById('characterName');
 const characterImg = document.getElementById('characterImg');
+const jokeText = document.getElementById('jokeText');
 
 const apiUrl = 'https://rickandmortyapi.com/api/character';
 /* This function gets a random character from the Rick and Morty API
     and updates the character card for display */
 // API key for OpenWeatherMap
 const weatherApi = `0735caea811b5cd9ce6a530e8faa946e`;
-
+// URL for Dad Joke API
+const dadJokeURL = `https://icanhazdadjoke.com/`;
 async function getRandCharacter() {
     try {
         // Getting total number of characters from API and generating random ID (API IDs start from 1)
@@ -27,6 +29,8 @@ async function getRandCharacter() {
         } else {
             earthWeather = `Character is not on Earth... Weather unknown.`;
         }
+        // Getting random dad joke
+        const randomJoke = await getDadJoke(); 
 
         // Updating character card
         characterImg.src = characterData.image;
@@ -37,11 +41,30 @@ async function getRandCharacter() {
         Location: ${characterData.location.name} <br>
         Number of Episodes appeared in: ${characterData.episode.length} <br>
         Weather: ${earthWeather}`;
+        // Updating joke section
+        jokeText.innerHTML = ''; // Clear previous joke
+        jokeText.textContent = randomJoke;
+        
     } catch (error) {
         console.error('Error fetching character data:', error);
         characterInfo.innerHTML = 'Could not receive character data.';
     }
 
+}
+async function getDadJoke() {
+    try {
+        const jokeResponse = await fetch(dadJokeURL, {
+            headers: {
+                'Accept': 'application/json',
+                'User-Agent': 'Rick and Morty JS Assignment (https://github.com/George-Baxter/Assignment3JS)'
+            }
+        });
+        const jokeData = await jokeResponse.json();
+        return jokeData.joke;
+    } catch (error) {
+        console.error('Error fetching dad joke:', error);
+        return 'Could not fetch dad joke.';
+    }
 }
 
 async function fetchWeather() {
